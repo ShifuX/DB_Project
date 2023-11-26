e86fa11fea125493cacdf110163d91c6280eafae
Gabriel <gabriel.anguiano13x@gmail.com>
2023-11-26 11:20:12 -0600

    established test for connection

--- database.js
+++ database.js
@@ -1,5 +1,21 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
+import express from "express";
+
+let port = 3001;
 
 // the URL needed to connect with the DB
 let dbURL = "";
 const connection = await mysql.createConnection(dbURL);
+
+const app = express();
+
+// retrieves all the pokemon in the DB
+app.get("/pokemons", async (req, res) => {
+  const query = "SELECT * FROM pokemon";
+  const [rows] = await connection.query(query);
+  res.send(rows);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
